d75fffaff0ea54a3fd8f58bcfbed748833e3bb91
夷则 <lsjlkz@gmail.com>
2022-06-17 18:27:30 +0800

    打包字节流

--- GameEngine/GENetPack.cpp
+++ GameEngine/GENetPack.cpp
@@ -8,6 +8,7 @@
 
 PackMessage::PackMessage() {
 	this->MsgSize = 0;
+	this->NewBuf();
 }
 
 PackMessage::~PackMessage() {
@@ -31,13 +32,19 @@ void PackMessage::NewBuf() {
 	this->curBufEmpty = MSG_MAX_SIZE;
 }
 
-bool PackMessage::PackBigObj(BigObj bigObj) {
-	// 打包类型
-	this->PackInt(ISBIGOBJ);
-	PackMessage* packObj = new PackMessage();
-	packObj->PackBigObj(bigObj);
-	// 打包长度
-	// TODO
+bool PackMessage::PackType(int t) {
+	this->PackCharInt(t);
+	return false;
+}
+
+bool PackMessage::PackCharInt(int c) {
+	char cc = static_cast<char>(c);
+	this->PackChar(cc);
+	return true;
+}
+
+bool PackMessage::PackChar(char c) {
+	this->PackByte(&c, sizeof(char));
 	return true;
 }
 
@@ -47,7 +54,7 @@ bool PackMessage::PackInt(int i) {
 }
 
 bool PackMessage::PackIntObj(int i) {
-	this->PackInt(ISINT);
+	this->PackType(IntFlag);
 	this->PackInt(i);
 	return true;
 }
@@ -58,18 +65,23 @@ bool PackMessage::PackLong(long l) {
 }
 
 bool PackMessage::PackLongObj(long l) {
-	this->PackInt(ISLONG);
+	this->PackType(LongFlag);
 	this->PackLong(l);
 	return true;
 }
 
 bool PackMessage::PackBool(bool b) {
-	this->PackByte(&b, sizeof(bool));
+	if(b){
+		char bb = static_cast<char>(TrueFlag);
+	}else{
+		char bb = static_cast<char>(FalseFlag);
+	}
+	this->PackByte(new char(FalseFlag), sizeof(char));
 	return true;
 }
 
 bool PackMessage::PackBoolObj(bool b) {
-	this->PackInt(ISBOOL);
+	this->PackType(BoolFlag);
 	this->PackInt(b);
 	return true;
 }
@@ -80,7 +92,7 @@ bool PackMessage::PackFloat(float f) {
 }
 
 bool PackMessage::PackFloatObj(float f) {
-	this->PackInt(ISFLOAT);
+	this->PackType(FloatFlag);
 	this->PackFloat(f);
 	return true;
 }
@@ -91,18 +103,18 @@ bool PackMessage::PackDouble(double d) {
 }
 
 bool PackMessage::PackDoubleObj(double d) {
-	this->PackInt(ISDOUBLE);
+	this->PackType(DoubleFlag);
 	this->PackDouble(d);
 	return true;
 }
 
-bool PackMessage::PackString(char *s, int size) {
+bool PackMessage::PackString(const char *s, int size) {
 	this->PackByte(s, size);
 	return true;
 }
 
-bool PackMessage::PackStringObj(char *s, int size) {
-	this->PackInt(ISSTRING);
+bool PackMessage::PackStringObj(const char *s, int size) {
+	this->PackType(StringFlag);
 	this->PackString(s, size);
 	return true;
 }
@@ -136,3 +148,58 @@ void PackMessage::PackByte(const void *pHead, int size) {
 	this->MsgSize += size;
 	this->curBufEmpty -= size;
 }
+
+bool PackMessage::PackLuaObj(lua_State *L) {
+	return false;
+}
+
+bool PackMessage::PackLuaHelp(lua_State *L, int index) {
+	this->curStackDeep++;		// 增加递归深度
+	int top = lua_gettop(L);
+	int lt = lua_type(L, index);
+	if(lt == LUA_TNIL){
+		// nil
+		this->PackType(NoneFlag);
+	}else if(lt == LUA_TBOOLEAN){
+		// bool
+		int b = lua_toboolean(L, index);
+		if(b){
+			this->PackType(TrueFlag);
+		}else{
+			this->PackType(FalseFlag);
+		}
+	}else if(lt == LUA_TNUMBER){
+		int i = lua_tonumber(L, index);
+		this->PackIntObj(i);
+	}else if(lt == LUA_TSTRING){
+		size_t size = 0;
+		const char* s = lua_tolstring(L, index, &size);
+		this->PackStringObj(s, size);
+	}else if(lt == LUA_TTABLE){
+		int& TableSize = this->PackIntRef();
+		int size = 0;
+		// 也就是把-1这些索引转为正值的索引，如长度为n，索引-2应为倒数第二个，也就是转为n-1
+		int table_index = lua_absindex(L, index);
+		// lua_checkstack检测栈上是否有足够的空间，如下是检测top上是否还有2个位置，不够的话会自动扩容
+		lua_checkstack(L, top + 2);
+		lua_pushnil(L);
+		ASSERT_LUA_TOP(L, top, 1);
+		while(lua_next(L, table_index)){
+			ASSERT_LUA_TOP(L, top, 2);
+			size++;
+			int ud = this->curStackDeep;
+			this->PackLuaHelp(L, -2);
+			GE_WIN_ASSERT(ud == this->curStackDeep);
+			ASSERT_LUA_TOP(L, top, 2);
+			this->PackLuaHelp(L, -1);
+		}
+	}
+	return false;
+}
+
+int &PackMessage::PackIntRef() {
+	int*r = new int(0);
+	this->PackInt(*r);
+	return *r;
+}
+
--- GameEngine/GENetPack.h
+++ GameEngine/GENetPack.h
@@ -7,23 +7,35 @@
 
 #include <vector>
 #include <list>
+#include "LuaEngine.h"
+#include "GEDefine.h"
 
-#define ISINT -1
-#define ISLONG -2
-#define ISBOOL -3
-#define ISFLOAT -4
-#define ISDOUBLE -5
-#define ISBIGOBJ -6
-#define ISSTRING -7
+#define IntFlag					-1
+#define LongFlag				-2
+#define BoolFlag				-3
+#define FloatFlag				-4
+#define DoubleFlag				-5
+#define StringFlag				-6
+#define CharFlag				-7
 
-#define MSG_MAX_SIZE 65536
+#define NoneFlag				-100
+#define TrueFlag				-101
+#define FalseFlag				-102
+#define TableFlag				-103
+#define BinFlag					-104
+#define DateTimeFlag			-105
+
+#define MSG_MAX_SIZE			65536		// 最大char长度
+#define MAX_STACK_DEEP			30			// 最大递归层数
+
+#define ASSERT_LUA_TOP(L, top, i)	\
+	if (top + i != lua_gettop(L)) { std::abort(); };
 
 typedef std::vector<char*>			BufPool;
 typedef std::list<char*>			BufQueue;
 
 // 这个模块即将会很庞大，貌似
 
-class BigObj;
 
 class PackMessage {
 
@@ -35,11 +47,16 @@ public:
 	char*			curBufHead;			// 当前打包的缓冲区的头
 	int 			curBufOffset;		// 当前打包的缓冲区偏移
 	int				curBufEmpty;		// 当前打包的缓冲区剩余空间
+	int				curStackDeep;		// 栈深度，防止递归自身
 public:
 	PackMessage();
 	~PackMessage();
 
+	bool			PackType(int t);
+	bool 			PackCharInt(int c);
+	bool 			PackChar(char c);
 	bool			PackInt(int i);
+	int&			PackIntRef();
 	bool			PackIntObj(int i);
 	bool			PackLong(long l);
 	bool			PackLongObj(long l);
@@ -49,9 +66,10 @@ public:
 	bool 			PackFloatObj(float f);
 	bool 			PackDouble(double d);
 	bool 			PackDoubleObj(double d);
-	bool 			PackString(char* s, int size);
-	bool 			PackStringObj(char* s, int size);
-	bool 			PackBigObj(BigObj bigObj);
+	bool 			PackString(const char* s, int size);
+	bool 			PackStringObj(const char *s, int size);
+	bool 			PackLuaObj(lua_State* L);
+	bool 			PackLuaHelp(lua_State* L, int index);
 	int				PackSize(){return MsgSize;}
 	void 			PackByte(const void* pHead, int size);
 	BufQueue*		BigMsgQueue(){return bigMsgQueue;}
@@ -69,14 +87,6 @@ public:
 	bool			UnpackBool(bool b);
 	bool 			UnpackFloat(float f);
 	bool 			UnpackDouble(double d);
-};
-
-class BigObj{
-private:
-	int ObjSize;
-public:
-	int Size(){return ObjSize;}
-
 };
 
 #endif //MYSLG_GENETPACK_H
